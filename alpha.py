7fc18e261cb7ece4fee49eea06c5bf26d23d3bd3
vetapalem <99022630+vetapalem@users.noreply.github.com>
2022-10-01 08:15:39 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,4 +1,4 @@
-print([chr(a) for a in range(65,65+26) if chr(a) !=  [x.capitalize() for x in 'aeiou' ]  ] )
-print([chr(a) for a in range(65,65+26) if chr(a) not in  [x.capitalize() for x in 'aeiou' ]  ] )
-print([chr(a) for a in range(65,65+26) if chr(a)  in  [x.capitalize() for x in 'aeiou' ]  ] )
+print([chr(a) for a in range(65,65+26) if chr(a) !=  [x.capitalize() for x in 'aeiou' ]  ] ) # total
+print([chr(a) for a in range(65,65+26) if chr(a) not in  [x.capitalize() for x in 'aeiou' ]  ] ) # with out vowels
+print([chr(a) for a in range(65,65+26) if chr(a)  in  [x.capitalize() for x in 'aeiou' ]  ] ) # with vowels
 
